f306ec884e70a85bf1e6bbaadb0664ae4e3a12b9
Lacayo <glacay30@nca.edu.ni>
2017-03-20 23:10:57 -0600

    Reworked foundation of Tetris and cleaned repository

--- src/Tetris/assets.py
+++ src/Tetris/assets.py
@@ -7,10 +7,17 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
-red_b = pygame.image.load('assets/images/red.png')
-orange_b = pygame.image.load('assets/images/orange.png')
-yellow_b = pygame.image.load('assets/images/yellow.png')
-green_b = pygame.image.load('assets/images/green.png')
-blue_b = pygame.image.load('assets/images/blue.png')
-teal_b = pygame.image.load('assets/images/teal.png')
-pink_b = pygame.image.load('assets/images/pink.png')
+directions = {'north': 0, 'east': 1, 'south': 2, 'west': 3}
+
+red_b = pygame.image.load('assets/images/tetras/red.png')
+orange_b = pygame.image.load('assets/images/tetras/orange.png')
+yellow_b = pygame.image.load('assets/images/tetras/yellow.png')
+green_b = pygame.image.load('assets/images/tetras/green.png')
+blue_b = pygame.image.load('assets/images/tetras/blue.png')
+teal_b = pygame.image.load('assets/images/tetras/teal.png')
+pink_b = pygame.image.load('assets/images/tetras/pink.png')
+
+# grid_b = pygame.image.load('assets/images/grid/grid_space.png')
+#
+# collision_b = pygame.image.load('assets/images/other/collision.png')
+# single_b = pygame.image.load('assets/images/other/single.png')
--- src/Tetris/main.py
+++ src/Tetris/main.py
@@ -1,50 +1,115 @@
 import pygame
-import classes as c
 import assets as a
 
-pygame.init()
 
-fps = 60
-width, height = 800, 608    # multiples of 16, the size of each block
-window = pygame.display.set_mode((width, height))   # creates surface 'window' and displays it to screen
-pygame.display.set_caption('Tetris')    # sets caption (words at top of window) to tetris
+class TetrisApplication:
+    def __init__(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.width, self.height = 800, 600
+        self.fps = 10
+        self.window = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption('Slither')
+        self.game_state = ['menu', 'game']
+        self.mainloop()
 
-grid1 = c.PlayingField('grid1', 10, 22)
-grid1.make_grid()
-test1 = c.TetraT('test1', window)
-test2 = c.TetraO('test2', window)
-test3 = c.TetraJ('test3', window)
-test4 = c.TetraL('test4', window)
-test5 = c.TetraS('test5', window)
-test6 = c.TetraZ('test6', window)
-test7 = c.TetraI('test7', window)
+    def mainloop(self):
+        while True:
+            self.event_handling()
+            if self.game_state[0] in 'menu':
+                self.menu_loop()
+            elif self.game_state[0] in 'game':
+                self.game_loop()
 
+    def game_loop(self):
+        self.window.fill(a.grey)
+        self.message(self.window, "Game window")
+        self.Block.render_all(self.window)
+        pygame.display.update()
 
-def game_menu():
-    menu = True
+    def menu_loop(self):
+        self.window.fill(a.white)
+        self.message(self.window, "Menu window")
+        pygame.display.update()
 
-    while menu:     # menu loop, if menu is false, quits function and quits program
-        for event in pygame.event.get():   # Event handler
-            if event.type == pygame.QUIT:  # quit from x button
-                menu = False
+    def event_handling(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:    # quit from pressing escape
-                    menu = False
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    quit()
+                if event.key == pygame.K_e:
+                    self.game_state.reverse()
 
-        window.fill(a.grey)
-        i = 50  # temp value for y because I'm lazy
-        test1.render(50, i)
-        test2.render(150, i)
-        test3.render(250, i)
-        test4.render(350, i)
-        test5.render(450, i)
-        test6.render(550, i)
-        test7.render(650, i)
+    class Block:
+        def __init__(self, surface, pos, block_type, rotation=0):
+            self.surface = surface
+            self.block_type = block_type
+            self.rotation = rotation
+            self.pos = tuple(pos)
+            self.size = 16
+            self.block_list = {
+                't': (['08-888', '8-88-8', '888-08', '08-88-08'], a.red_b),
+                's': (['088-88', '8-88-08'], a.pink_b),
+                'z': (['88-088', '08-88-8'], a.blue_b),
+                'o': (['88-88'], a.yellow_b),
+                'i': (['8-8-8-8', '8888'], a.teal_b),
+                'j': (['08-08-88', '8-888', '88-8-8', '888-008'], a.green_b),
+                'l': (['8-8-88', '888-8', '88-08-08', '008-888'], a.orange_b)
+            }
+            self.render_block()
 
-        pygame.display.update()     # THIS GOES AFTER EVERYTHING HAS BEEN LOADED
-        pygame.time.Clock().tick(fps)   # determines frame-rate
+        def render_block(self):
+            base = self.block_list[self.block_type][0][self.rotation]
+            color = self.block_list[self.block_type][1]
+            unpacked = self.unpack(base, self.pos)
+            for coords in unpacked:
+                self.surface.blit(color, (coords[0] * self.size, coords[1] * self.size))
 
-game_menu()
+        @staticmethod
+        def unpack(base, coordinate):
+            x, y, complete = 0, 0, []
+            for char in base:
+                if char in '0':
+                    x += 1
+                elif char in '8':
+                    complete.append([coordinate[0] + x, coordinate[1] + y])
+                    x += 1
+                elif char in '-':
+                    x = 0
+                    y += 1
+            return complete
 
-pygame.quit()
-quit()
+        @classmethod
+        def render_all(cls, surface):
+            b1 = cls(surface, [0, 3], 't', 0)
+            b2 = cls(surface, [0, 8], 't', 1)
+            b3 = cls(surface, [0, 13], 't', 2)
+            b4 = cls(surface, [0, 18], 't', 3)
+            b5 = cls(surface, [5, 3], 's', 0)
+            b6 = cls(surface, [5, 8], 's', 1)
+            b7 = cls(surface, [10, 3], 'z', 0)
+            b8 = cls(surface, [10, 8], 'z', 1)
+            b9 = cls(surface, [15, 3], 'o', 0)
+            b10 = cls(surface, [20, 3], 'i', 0)
+            b11 = cls(surface, [20, 8], 'i', 1)
+            b12 = cls(surface, [25, 3], 'j', 0)
+            b13 = cls(surface, [25, 8], 'j', 1)
+            b14 = cls(surface, [25, 13], 'j', 2)
+            b15 = cls(surface, [25, 18], 'j', 3)
+            b16 = cls(surface, [30, 3], 'l', 0)
+            b17 = cls(surface, [30, 8], 'l', 1)
+            b18 = cls(surface, [30, 13], 'l', 2)
+            b19 = cls(surface, [30, 18], 'l', 3)
+
+    @staticmethod
+    def message(surface, msg="Text", color=(0, 0, 0), size=40, pos=(0, 0)):
+        font = pygame.font.SysFont(None, size)
+        text_surface = font.render(msg, True, color)
+        surface.blit(text_surface, pos)
+
+if __name__ in '__main__':
+    app = TetrisApplication()
