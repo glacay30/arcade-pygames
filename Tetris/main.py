51b12b19b61687fc789bd08a4d8819facaf87805
Lacayo <glacay30@nca.edu.ni>
2017-05-09 23:52:30 -0600

    Finished Tetris

--- Tetris/main.py
+++ Tetris/main.py
@@ -1,4 +1,3 @@
-import assets as a
 import block as b
 import grid as g
 import menu as m
@@ -6,24 +5,53 @@ import pygame
 
 
 class TetrisApplication:
-    def __init__(self, initial_state):
+    def __init__(self, initial_state='menu'):
         pygame.init()
         self.clock = pygame.time.Clock()
         self.time_start = 0
         self.time_stop = 60
         self.time_step = 1
 
-        self.width, self.height = 240, 400
-        self.window = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption('Tetris')
+        info_object = pygame.display.Info()
+        self.window_width, self.window_height = info_object.current_w, info_object.current_h
+        self.window = pygame.display.set_mode((self.window_width, self.window_height), pygame.FULLSCREEN)
 
-        self.state_current = initial_state
-        # total states: 'menu', 'game', 'pause', 'game over'
+        # self.window_width, self.window_height = 800, 600
+        # self.window = pygame.display.set_mode((self.window_width, self.window_height))
+
+        pygame.display.set_caption('Tetris')
+        pygame.display.set_icon(pygame.image.load_extended('images/icon.gif'))
+
+        self.state_current = initial_state  # total states: 'menu', 'game', 'pause', 'game over'
         self.score = 0
-        self.offset = (2, 3)
-        self.stone = b.Stone(self.window, (3, 0), [3, 0], self.offset, 't', 0)
-        self.grid = g.Grid(self.window, (10, 20), self.offset)
-        self.menu = m.Menu(self.window, self.message)
+
+        grid_dimensions = (10, 20)
+        offset = (self.window_width // 2 - ((grid_dimensions[0] * int(self.window_height * (13 / 300))) // 2)), \
+                 (self.window_height // 2 - ((grid_dimensions[1] * int(self.window_height * (13 / 300))) // 2))
+
+        img_scale = int(self.window_height * (13 / 300))
+        self.font_scale_x = int(self.window_width * (47 / 1366))
+        self.font_scale_y = int(self.window_width * (16 / 768))
+
+        self.stone = b.Stone(surface=self.window,
+                             scale=img_scale,
+                             offset=offset,
+                             pos=(grid_dimensions[0] // 2, 0),
+                             block_type='t',
+                             rotation=0
+                             )
+
+        self.grid = g.Grid(surface=self.window,
+                           scale=img_scale,
+                           offset=offset,
+                           grid_dimensions=grid_dimensions
+                           )
+
+        self.menu = m.Menu(surface=self.window,
+                           window_dimensions=(self.window_width, self.window_height),
+                           message_func=self.message
+                           )
+
         pygame.mixer.music.load('audio/tetris_theme.ogg')
         pygame.mixer.music.play(-1)
         pygame.mixer.music.pause()
@@ -36,7 +64,7 @@ class TetrisApplication:
                 pygame.quit()
                 quit()
 
-            # this restricts the input based on what self.state_current currently is
+            # the state_current determines what actions are available
             if self.state_current in 'menu':
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN:
@@ -53,24 +81,24 @@ class TetrisApplication:
                         if event.key == pygame.K_ESCAPE:
                             self.state_current = 'menu'
                             self.__init__('menu')
-                        if event.key == pygame.K_a:
-                            self.stone.move('left', self.grid)
-                        if event.key == pygame.K_d:
-                            self.stone.move('right', self.grid)
-                        if event.key == pygame.K_s:
-                            self.stone.move('down', self.grid)
-                        if event.key == pygame.K_SPACE:
-                            self.stone.drop(self.grid)
                         if event.key == pygame.K_LEFT:
-                            self.stone.rotate('ccw', self.grid)
+                            self.stone.move('left', self.grid)
                         if event.key == pygame.K_RIGHT:
+                            self.stone.move('right', self.grid)
+                        if event.key == pygame.K_DOWN:
+                            self.stone.drop(self.grid)
+                        if event.key == pygame.K_UP:
                             self.stone.rotate('cw', self.grid)
-                        if event.key == pygame.K_t:
+                        if event.key == pygame.K_RETURN:
+                            self.state_current = 'pause'
+                        elif event.key == pygame.K_KP_ENTER:
                             self.state_current = 'pause'
 
             elif self.state_current in 'pause':
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_t:
+                    if event.key == pygame.K_RETURN:
+                        self.state_current = 'game'
+                    elif event.key == pygame.K_KP_ENTER:
                         self.state_current = 'game'
 
             elif self.state_current in 'game over':
@@ -104,10 +132,10 @@ class TetrisApplication:
             self.state_current = 'game'
 
     def state_game(self):
-        self.window.fill(a.color_black)
+        self.window.fill((0, 0, 0))
         self.grid.render_grid()
         self.stone.render_block()
-        self.message(str(self.score), pos=[5, self.height - 30])
+        self.message(str(self.score), 3, (255, 255, 255), center='y', offset_arg=(2, 0))
 
         if self.time_tick():
             self.stone.move('down', self.grid, reset='enable')
@@ -119,13 +147,13 @@ class TetrisApplication:
             self.time_step += 0.2
 
     def state_pause(self):
-        self.message("Paused!", center=(1, 1))
+        self.message("Paused!", 1, (255, 255, 255), center='xy')
 
     def state_game_over(self):
-        self.message('Game Over!', size=50, center=(1, 0), pos=(0, 20))
-        self.message('SPACE to reset', size=30, center=(1, 0), pos=(0, 70))
-        self.message('ESCAPE to go to menu', size=30, center=(1, 0), pos=(0, 100))
-        self.message('BACKSPACE to exit', size=30, center=(1, 0), pos=(0, 130))
+        self.message('Game Over!', 3, (200, 200, 200), center='x', offset_arg=(0, 1))
+        self.message('SPACE to reset', 2, (200, 200, 200), center='x', offset_arg=(0, 5))
+        self.message('ESCAPE to go to menu', 2, (200, 200, 200), center='x', offset_arg=(0, 7))
+        self.message('BACKSPACE to exit', 2, (200, 200, 200), center='x', offset_arg=(0, 9))
 
     def time_tick(self):
         if self.time_start >= self.time_stop:
@@ -134,19 +162,23 @@ class TetrisApplication:
         else:
             self.time_start += int(self.time_step)
 
-    def message(self, msg, color=(255, 255, 255), size=40, pos=(0, 0), center=(0, 0)):
-        font = pygame.font.SysFont(None, size)
-        text_surface = font.render(msg, True, color)
+    def message(self, msg, font_size, font_color, pos=(0, 0), center='', offset_arg=(0, 0)):
+
+        font = pygame.font.SysFont(None, font_size * self.font_scale_x)
+        text_surface = font.render(msg, True, font_color)
         text_width, text_height = font.size(msg)
 
-        if center[0] == 1 and center[1] == 1:
-            self.window.blit(text_surface, (self.width // 2 - text_width // 2, self.height // 2 - text_height // 2))
-        elif center[0] == 1:
-            self.window.blit(text_surface, (self.width // 2 - text_width // 2, pos[1]))
-        elif center[1] == 1:
-            self.window.blit(text_surface, (pos[0], self.height // 2 - text_height // 2))
-        else:
-            self.window.blit(text_surface, pos)
+        offset = offset_arg[0] * self.font_scale_x * font_size, offset_arg[1] * self.font_scale_y * font_size
+        placement = pos[0] + offset[0], pos[1] + offset[1]
+
+        if center in 'x':
+            placement = self.window_width // 2 - text_width // 2, pos[1] + offset[1]
+        elif center in 'y':
+            placement = pos[0] + offset[0], self.window_height // 2 - text_height // 2
+        elif center in 'xy':
+            placement = self.window_width // 2 - text_width // 2, self.window_height // 2 - text_height // 2
+
+        self.window.blit(text_surface, placement)
 
 if __name__ in '__main__':
-    app = TetrisApplication('menu')
+    app = TetrisApplication()
